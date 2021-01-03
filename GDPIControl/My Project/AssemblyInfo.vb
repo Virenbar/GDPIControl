fdc1d13a476c42a23765a34b6591218aeacdffac
Artyom <rib.artem@gmail.com>
2021-01-03 21:07:21 +0500

    Remove VS junk

--- GDPIControl/My Project/AssemblyInfo.vb
+++ GDPIControl/My Project/AssemblyInfo.vb
@@ -2,12 +2,6 @@
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' Общие сведения об этой сборке предоставляются следующим набором
-' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
-' общие сведения об этой сборке.
-
-' Проверьте значения атрибутов сборки
-
 <Assembly: AssemblyTitle("GDPIControl")>
 <Assembly: AssemblyDescription("GoodbyeDPI Tray Control")>
 <Assembly: AssemblyCompany("")>
@@ -16,19 +10,6 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
-
-'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 <Assembly: Guid("9cf720fb-c677-48bc-867b-a9358a06bb95")>
-
-' Сведения о версии сборки состоят из следующих четырех значений:
-'
-'      Основной номер версии
-'      Дополнительный номер версии
-'   Номер сборки
-'      Редакция
-'
-' Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
-' используя "*", как показано ниже:
-' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.0.0")>
